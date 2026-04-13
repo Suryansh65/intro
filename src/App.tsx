255b683c7dc96084a2105406258a77f237ebaea7
suryansh <sysingh@avyro.in>
2026-04-13 10:02:52 +0530

    React_intro

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import Anyname from "./Intro/intro";
+// No need to give .tsx extension -> react will handle automatically
+
+function App(){
+  return (
+    <div>
+      <Anyname />
+      <MyProfile />
+      <ListItems />
+    </div>
+  )
+}
+
+export default App;
+
+// export -> Named export , allow multiple components to export also use {} while importing the components, can also rename while importing, must use exact name of the component while importing
+
+// import {MyButton as btn} from './Intro/intro'
+
+// export default -> Default export -> only one allowed, can be import with any Name
+// import Anyname from './intro';
+
+// JSX -> Javascript XML : It let's you write HTML like syntax inside JS. Behind the scene a tool called Babel compiles it into regular JS before the browser runs it.
+
+/*
+Key JSX rule to Remember:
+1. Use className instead of class
+<div className="container">
+
+2. Self-close tags that have no children
+<MyButton />
+<img src="photo.png" />
+
+3. Use curly braces {} for JavaScript expressions
+<h1>{userName}</h1>
+
+4. Must return a single parent element 
+return (
+  <div> {one parent wrap everything}
+  <h1>Hello</h1>
+  <p>world</p>
+  </div>
+)
+
+*/
+
+const user = {
+  name: 'John Doe',
+  imageUrl: 'https://react.dev/images/docs/scientists/yXOvdOSs.jpg',
+  imagSize: 90,
+}
+
+export function MyProfile(){
+  return(
+    <div>
+      <h1>{user.name}</h1>
+      <img 
+      src={user.imageUrl}
+      className="avatar"
+      alt={'Photo of' + user.name}
+      style={{
+        width: user.imagSize,
+        height:user.imagSize
+      }}
+      
+      />
+    </div>
+  )
+}
+
+/*using for loop to display items */
+const products = [
+  {title: 'Cabbage',id:1},
+  {title: 'Garlic',id:2},
+  {title: 'Apple',id:3},
+]
+
+export function ListItems(){
+  const listItems = products.map(product =>
+    <li key={product.id} >
+      {product.title}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  )
+}
---
+++ src/Intro/intro.tsx
@@ -0,0 +1,14 @@
+export function MyButton(){
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default function MyApp(){
+  return (
+    <div>
+      <h1>Welcome to my App</h1>
+      <MyButton/>
+    </div>
+  )
+}
