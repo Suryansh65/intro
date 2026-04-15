66579656ec42f5e5486eaeee05641c0a279c74eb
suryansh <sysingh@avyro.in>
2026-04-15 10:22:24 +0530

    Learn about React render and wrong use cases of useState() deeply

--- src/Intro/hooks.tsx
+++ src/Intro/hooks.tsx
@@ -8,6 +8,22 @@ Remember:
 -It can't be used in event handler, callbacks or any other helper function
 
 */
+
+/*
+    React render: render means just to call Js functions to return JSX template
+    React maintain Hook order list: to maintain order of hooks,
+    Imp: It track each component hooks, if number of hooks per component differs than it give error but if number of hooks per component remains same than react is happy.
+
+    It didn't care about number of components - Just number of hooks inside Component remains same.
+
+*/
+
+/*
+Never change state while render - It will cause Infinite calling
+useEffect without dependency array may cause infinite calling
+useEffect with state that changes frequently
+object as dependency inside function cause infinite render as react take that object as reference not by value so each render the object is different, sol: Move them outside component or use useMemo();
+*/
 import { useState } from "react";
 function Hooks(){
     return (
@@ -28,11 +44,10 @@ function InvalidHooks(){
             <button onClick={HandleClick} >Click me {count}</button>
         </div>
     )
+}
     // const isLoggedIn = true;
     // if(isLoggedIn){
         // setCount can't be used here as it may cause infinite renders:  setCount queues re-render which may cause infinite loops
         // setCount(count + 1);
         // console.log(count);
     // }
-
-}
